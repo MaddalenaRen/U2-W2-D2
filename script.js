d9b62133e79bd8e86c000f0974566dea79e7e71f
MaddalenaRen <ileniarennella@gmail.com>
2025-03-12 00:57:43 +0100

    complete

--- script.js
+++ script.js
@@ -8,4 +8,18 @@ hot.forEach((card, i) => {
     label.classList.add('position-absolute')
     hot[i].classList.add('position-relative')
     hot[i].appendChild(label)
+
+    label.style.top = '5px'
+    label.style.top = '5px'
+
 })
+
+document.getElementById("contaViaggi").addEventListener("click", function () {
+    let viaggi = document.querySelectorAll(".viaggio").length;
+    alert(`Ci sono ${viaggi} viaggi disponibili.`);
+});
+
+document.getElementById("rimuoviCard").addEventListener("click", function () {
+    let cards = document.querySelectorAll(".card");
+    cards.forEach(card => card.remove());
+});
