c7f6f0e5e7151cd5cb39ea466c8d84c07b91445a
MaddalenaRen <ileniarennella@gmail.com>
2025-03-15 01:23:18 +0100

    update

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 
-const hot = document.querySelectorAll("#hot .card")
+const hot = document.querySelectorAll("#hot .card");
 
 hot.forEach((card, i) => {
-    const label = document.createElement('p')
-    label.innerText = 'Hot'
-    label.classList.add('bg-danger', 'text-light')
-    label.classList.add('position-absolute')
-    hot[i].classList.add('position-relative')
-    hot[i].appendChild(label)
+    const label = document.createElement('p');
+    label.innerText = 'Hot';
+    label.classList.add('bg-danger', 'text-light');
+    label.classList.add('position-absolute');
+    hot[i].classList.add('position-relative');
+    hot[i].appendChild(label);
 
-    label.style.top = '5px'
-    label.style.top = '5px'
+    label.style.top = '5px';
+    label.style.top = '5px';
 
-})
+});
 
 document.getElementById("contaViaggi").addEventListener("click", function () {
     let viaggi = document.querySelectorAll(".viaggio").length;
